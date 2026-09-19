90224793f463d12d556b00d522d312d17ae8cfb0
gycha0109-beep <gycha0109@gmail.com>
2026-09-19 20:42:34 +0900

    test(db): run finalizer catalog blocker guard

--- test/db/run_authority_core.sh
+++ test/db/run_authority_core.sh
@@ -56,6 +56,7 @@ bash test/db/reading_clarification_concurrency.sh
 bash test/db/notification_delivery_attempt_concurrency.sh
 bash test/db/account_deletion_start_concurrency.sh
 bash test/db/account_deletion_finalization_preflight.sh
+bash test/db/account_deletion_finalizer_catalog_guard.sh
 
 run_isolated_case myeongha_privacy_reconciliation_replay_test bash test/db/privacy_reconciliation_replay.sh
 
